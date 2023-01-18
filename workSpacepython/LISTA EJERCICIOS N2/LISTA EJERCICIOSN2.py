2e392cbb1adc69a9ea9ccacba95772b2577a3a6d
slvhank1201 <122219154+slvhank1201@users.noreply.github.com>
2023-01-19 00:14:22 +0100

    Update LISTA EJERCICIOSN2.py

--- workSpacepython/LISTA EJERCICIOS N2/LISTA EJERCICIOSN2.py
+++ workSpacepython/LISTA EJERCICIOS N2/LISTA EJERCICIOSN2.py
@@ -1,6 +1,37 @@
 #LISTA EJERCICIOS N2
-size=7
-for i in range(0,size): #para este ejercicio se usan ciclos anidados
-    for j in range(0,size):
-        print ('* ', end='')
-    print()
+#PREGUNTA1
+while True:
+    print("Bienvenido")
+    print("""
+a)Dibujar un cuadrado
+b)Identificar un múltiplo de 2
+c)Identificar mayores de edad en una lista
+d)Salir """)
+    action1=input("Ingresar opción para llevar a cabo: ").lower()
+    if action1=="a":
+     size=6
+     for i in range(0,size):
+        for j in range(0,size):
+             print('* ', end=" ")
+        print()
+    elif action1=="b":
+     lista={882, 9874, 9873, 8765, 98348}
+     print("Los números múltiplos de 2 son: ")
+     for i in lista:
+        if i%2==0:
+            print(i)
+        else:
+            False
+    elif action1=="c":
+        listaPersonas ={'Álvaro':12, 'Guzmán':17, 'Steven':21,'Fabrizio':18}
+        print("Los mayores de edad son: ")
+        for j,k in listaPersonas.items():
+            if k>=18:
+                print(j)
+            else:
+                False
+    elif action1=="d":
+        print("Programa finalizado. ¡Hasta luego!")
+        break
+    else:
+        print("Comando desconocido. Vuelve a intentarlo")
