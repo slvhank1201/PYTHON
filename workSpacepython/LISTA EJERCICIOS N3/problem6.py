5e2149b0f4262f9fb23ee93b066eb0e896e69484
slvhank1201 <122219154+slvhank1201@users.noreply.github.com>
2023-01-26 06:34:34 +0100

    actualizacion 26/01/2023
    ejercicio 6 done

---
+++ workSpacepython/LISTA EJERCICIOS N3/problem6.py
@@ -0,0 +1,4 @@
+import os
+rute=r'C:\Users\tyaro\OneDrive\Escritorio\PYTHON\workSpacepython\LISTA EJERCICIOS N3\problem6.py'
+rut=os.path.basename(rute)
+print("El nombre del archivo en ejecución es: ", rut)
